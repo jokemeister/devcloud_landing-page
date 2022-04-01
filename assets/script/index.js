7d6e3655b61b8e16ecf6e1aab198de89bc66aed0
Daniil Dubrovskyi <ddi.dubrovskyi@gmail.com>
2022-04-02 00:30:28 +0300

    topper

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,5 +1,7 @@
 import popUp from "./component/pop-up.js"
+import btnBurgerChecked from "./component/btn-burger.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     popUp();
+    btnBurgerChecked();
 })
