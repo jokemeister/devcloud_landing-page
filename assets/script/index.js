9f35c661eebedd91e9b0fa567bac849132a621a6
Daniil Dubrovskyi <ddi.dubrovskyi@gmail.com>
2022-04-05 11:44:56 +0300

    footer

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,13 +1,7 @@
 import popUp from "./component/pop-up.js"
-<<<<<<< HEAD
 import btnBurgerChecked from "./component/btn-burger.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     popUp();
     btnBurgerChecked();
-=======
-
-document.addEventListener('DOMContentLoaded', () => {
-    popUp();
->>>>>>> header
 })
