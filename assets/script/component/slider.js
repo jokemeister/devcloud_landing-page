b76ee823380ce31e685381f5f9fcd0c25c715b2a
Daniil Dubrovskyi <ddi.dubrovskyi@gmail.com>
2022-04-12 17:00:22 +0300

    services_basis

--- assets/script/component/slider.js
+++ assets/script/component/slider.js
@@ -3,15 +3,12 @@ import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
 const slider = () => {
     // swiper Stats
     if (window.innerWidth <= 480) {
-      let statsSlider = new Swiper(".mySwiper", {
+      let statsSlider = new Swiper(".js--swiper-stats", {
         enabled: true,
         slidesPerView: 2,
         spaceBetween: 0,
         slidesPerGroup: 2,
-        enabled: true,
         rewind: true,
-        // loop: true,
-        // loopFillGroupWithBlank: true,
         pagination: {
           bulletActiveClass: 'swiper-stats-pagination-bullet-is-active',
           bulletClass: 'swiper-stats-pagination-bullet',
@@ -23,15 +20,36 @@ const slider = () => {
         },
       });
     } else {
-      let statsSlider = new Swiper(".mySwiper", {
+      let statsSlider = new Swiper(".js--swiper-stats", {
         slidesPerView: 4,
         enabled: false,
       });
       let pagination = document.querySelector('.swiper-stats-pagination');
       pagination.classList.add('non-active');
     }
-
     // /swiper Stats
+
+    // swiper Services
+
+    if (window.innerWidth <= 480) {
+      let servicesSlider = new Swiper(".js--swiper-services", {
+        enabled: true,
+        slidesPerView: 2,
+        spaceBetween: 30,
+        centeredSlides: true,
+        pagination: {
+          el: ".swiper-services-pagination",
+          clickable: true,
+        },
+      });
+    } else {
+      let servicesSlider = new Swiper(".js--swiper-services", {
+        slidesPerView: 4,
+        enabled: false,
+      });
+    }
+
+    // /swiper Services
 }
 
 export default slider;
