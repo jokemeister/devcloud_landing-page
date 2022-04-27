38e4bddd235a6a055443e77f02e02d78f343aff9
Daniil Dubrovskyi <ddi.dubrovskyi@gmail.com>
2022-04-27 21:56:14 +0300

    slider.js with breakpoints

--- assets/script/component/slider.js
+++ assets/script/component/slider.js
@@ -2,8 +2,8 @@ import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
 
 const slider = () => {
     // swiper Stats
-    if (window.innerWidth <= 480) {
-      let statsSlider = new Swiper(".js--swiper-stats", {
+
+    let statsSlider = new Swiper (".js--swiper-stats", {
         enabled: true,
         slidesPerView: 2,
         spaceBetween: 0,
@@ -18,52 +18,46 @@ const slider = () => {
             return '<div class="' + className + '"' + 'data-index="' + (index + 1) + '">' + "</div>";
           },
         },
-      });
-    } else {
-      let statsSlider = new Swiper(".js--swiper-stats", {
-        slidesPerView: 4,
-        enabled: false,
-      });
-      let pagination = document.querySelector('.swiper-stats-pagination');
-      pagination.classList.add('non-active');
-    }
+        breakpoints: {
+            480: {
+                slidesPerView: 4,
+                enabled: false
+            }
+        }
+    })
+    let pagination = document.querySelector('.swiper-stats-pagination');
+    pagination.classList.add('non-active');
     // /swiper Stats
 
     // swiper Services
-
-    if (window.innerWidth <= 768 && window.innerWidth > 500) {
-      let servicesSlider = new Swiper(".js--swiper-services", {
-        enabled: true,
-        slidesPerView: 2,
-        spaceBetween: 30,
-        centeredSlides: true,
-        loop: true,
-        pagination: {
-          bulletActiveClass: 'swiper-services-pagination-bullet-is-active',
-          bulletClass: 'swiper-services-pagination-bullet',
-          el: ".swiper-services-pagination",
-          clickable: true,
-          renderBullet: function(index, className) {
-            return '<div class="' + className + '"' + 'data-index="' + (index + 1) + '">' + "</div>";
-          },
-        },
-      });
-    } else if (window.innerWidth <= 500){
-      let servicesSlider = new Swiper(".js--swiper-services", {
+    let servicesSlider = new Swiper(".js--swiper-services", {
         slidesPerView: 1,
         enabled: false,
-      });
-      let pagination = document.querySelector('.swiper-services-pagination');
-      pagination.classList.add('non-active');
-    } else {
-      let servicesSlider = new Swiper(".js--swiper-services", {
-        slidesPerView: 4,
-        enabled: false,
-      });
-      let pagination = document.querySelector('.swiper-services-pagination');
-      pagination.classList.add('non-active');
-    }
-
+        breakpoints: {
+            500: {
+                enabled: true,
+                slidesPerView: 2,
+                spaceBetween: 30,
+                centeredSlides: true,
+                loop: true,
+                pagination: {
+                  bulletActiveClass: 'swiper-services-pagination-bullet-is-active',
+                  bulletClass: 'swiper-services-pagination-bullet',
+                  el: ".swiper-services-pagination",
+                  clickable: true,
+                  renderBullet: function(index, className) {
+                    return '<div class="' + className + '"' + 'data-index="' + (index + 1) + '">' + "</div>";
+                  }
+                }
+            },
+            768: {
+                slidesPerView: 4,
+                enabled: false
+            }
+        }
+    })
+    let pagination = document.querySelector('.swiper-services-pagination');
+    pagination.classList.add('non-active');
     // /swiper Services
 }
 
